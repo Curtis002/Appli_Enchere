f6916e9cd1a49c3122dd5d7bdc9fbb0bdac9a789
DESKTOP-7RBMLGI\Asus <anthony.romian2021@campus-eni.fr>
2021-05-17 14:24:44 +0200

    commit add class article vendu

---
+++ src/main/java/fr/eni/appli_enchere/bo/ArticleVendu.java
@@ -0,0 +1,14 @@
+package fr.eni.appli_enchere.bo;
+
+import java.util.Date;
+
+public class ArticleVendu {
+	private Integer noArticle;
+	private String nomArticle;
+	private String description;
+	private Date dateDebutEncheres;
+	private Date dateFinEncheres;
+	private int miseAPrix;
+	private int prixVente;
+	private int etatVente;
+}
